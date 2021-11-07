07e70c7c8556b794601093678df0f65101025347
cmrotruck <crotruck@gmail.com>
2021-11-07 18:13:57 -0500

    added fight round

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -1,12 +1,46 @@
 var playerName = window.prompt("What is your robot's name?");
-//creating a function named "fight"
-function fight(){
-    window.alert("The fight has begun!");
-}
-console.log(playerName);
-// this will do math and log 20
-console.log(10 + 10);
+var playerHealth = 100;
+var playerAttack = 10;
 
-//what is this?
-console.log("Our robot's name is " + playerName);
-// fight();
+// you can also log multiple values at once like this
+console.log(playerName,playerAttack,playerHealth);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+//creating a function named "fight"
+var fight = function(){
+    window.alert("Wecome to Robot Gladiators!");
+    
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable.
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    
+    //check enemy's health
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died!");
+    }
+    else{
+        window.alert(enemyName + " still has " + enemyHealth + " health remaining.");
+    }
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    //check enemy's health
+    if (playerHealth <= 0){
+        window.alert(playerName + " has died!");
+    }
+    else{
+        window.alert(playerName + " still has " + playerHealth + " health remaining.");
+    }
+
+};
+
+
+
+fight();
